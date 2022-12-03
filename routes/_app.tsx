197b331d9f44ac01e8c2351a18ffb414ccaf40d6
Robin Steeman <robin@tracefy.com>
2022-12-03 23:37:23 +0100

    added twitter meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,12 +6,22 @@ const Head = () => {
         <HtmlHead>
             <title>Robin's fresh blog</title>
             <meta property="og:site_name" content="Robin's fresh blog"/>
+            <meta name="description" content="Read my thoughts or look at my top lists" />
             <meta property="og:title" content="Robin's fresh blog"/>
-            <meta property="og:description" content="Blog and top lists" />
+            <meta property="og:url" content="https://blog.robinsteeman.com/" />
+            <meta property="og:description" content="Read my thoughts or look at my top lists" />
             <meta property="og:image" itemProp="image"
                   content="https://blog.robinsteeman.com/kaneda.png"/>
             <meta property="og:type" content="website"/>
             <meta property="og:updated_time" content="1670106416090"/>
+
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://blog.robinsteeman.com/" />
+            <meta property="twitter:title" content="Robin's fresh blog" />
+            <meta property="twitter:description" content="Read my thoughts or look at my top lists" />
+            <meta property="twitter:image" content="https://blog.robinsteeman.com/kaneda.png"/>
+
             <link rel="icon" href="/kaneda.png" />
         </HtmlHead>
     )
