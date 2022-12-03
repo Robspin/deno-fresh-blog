284c62cd70f79c94692995e847c8f59c0c7ca3c2
Robin Steeman <robin@tracefy.com>
2022-12-03 23:27:24 +0100

    added meta tags

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,13 @@ const Head = () => {
     return (
         <HtmlHead>
             <title>Robin's fresh blog</title>
-            <meta content="Blog and top lists" name="description" />
+            <meta property="og:site_name" content="Robin's fresh blog"/>
+            <meta property="og:title" content="Robin's fresh blog"/>
+            <meta property="og:description" content="Blog and top lists" />
+            <meta property="og:image" itemProp="image"
+                  content="https://blog.robinsteeman.com/kaneda.png"/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:updated_time" content="1670106416090"/>
             <link rel="icon" href="/kaneda.png" />
         </HtmlHead>
     )
