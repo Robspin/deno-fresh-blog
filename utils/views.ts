7ddae0e4f44c6dcd639559a104dcf1481b79c978
Robin Steeman <robin@tracefy.com>
2023-02-19 14:54:03 +0100

    test

--- utils/views.ts
+++ utils/views.ts
@@ -1,11 +1,7 @@
-import { load } from "https://deno.land/std@0.177.0/dotenv/mod.ts"
-const env = await load()
-
 export const updateAndGetViews = async (): Promise<number> => {
     let views = 0
     try {
-        console.log('env:', env)
-        const res = await fetch(`${env.DENO_PRISMA_AKIRA_API_URL}/pageviews/BLOG_ROBINSTEEMAN`, { method: "PUT" })
+        const res = await fetch(`${Deno.env.get("DENO_PRISMA_AKIRA_API_URL")}/pageviews/BLOG_ROBINSTEEMAN`, { method: "PUT" })
         const data = await res.json()
         views = data.data.views
     } catch (e) {
