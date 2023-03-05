37305f29899fc267041ac80724903fbb200fe2a5
Robin Steeman <robin@tracefy.com>
2023-03-05 14:22:15 +0100

    testerino

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+console.log('pre import')
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
@@ -11,4 +12,7 @@ import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
 console.log('pre server')
+
 await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+
+console.log('post server')
