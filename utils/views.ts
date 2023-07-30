5e418986c3bb9b5719012430640445acca8c006a
Robspin <robin@tracefy.com>
2023-07-30 08:05:41 +0200

    update env key

--- utils/views.ts
+++ utils/views.ts
@@ -1,7 +1,7 @@
 export const getViews = async (): Promise<number> => {
     let views = 0
     try {
-        const res = await fetch(`${Deno.env.get("DENO_VIEWS_API_URL")}?key=blog`, { method: "GET" })
+        const res = await fetch(`${Deno.env.get("VIEWS_API_URL")}?key=blog`, { method: "GET" })
         views = await res.json()
     } catch (e) {
         console.log('here: ', e)
